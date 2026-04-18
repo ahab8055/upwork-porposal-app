1a0a3084366acc2894b5c3e2199cc8fa9eccc2d9
Muhammad Ahab <muhammadahab8055@gmail.com>
2026-04-18 19:05:54 +0500

    Suspention added

--- src/app/(auth)/accept-invite/page.tsx
+++ src/app/(auth)/accept-invite/page.tsx
@@ -225,7 +225,7 @@ function AcceptInviteContent() {
 
 export default function AcceptInvitePage() {
   return (
-    <Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Loading...</div>}>
       <AcceptInviteContent />
     </Suspense>
   );
